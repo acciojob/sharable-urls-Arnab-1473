14d3f5e209832a7a9e5bc43115d57572842b5a9a
Arnab-1473 <arban.nilr.1473@gmail.com>
2023-02-16 13:13:07 +0530

    updated

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // your code here
+const nameInput = document.getElementById('name');
+const yearInput = document.getElementById('year');
+const urlText = document.getElementById('url');
+const btn = document.getElementById('button');
+ 
+btn.addEventListener('click', () =>{
+    if(nameInput.value.length>0 && yearInput.value.length <= 0){
+        urlText.innerHTML = `https://localhost:8080/`;
+        urlText.innerHTML += `?name=${nameInput.value}`;
+    }
+    if(yearInput.value.length >0 && nameInput.value.length<= 0){
+        urlText.innerHTML = `https://localhost:8080/`;
+        urlText.innerHTML += `?year=${yearInput.value}`;
+    }
+    if(yearInput.value.length >0 && nameInput.value.length> 0){
+        urlText.innerHTML = `https://localhost:8080/`;
+        urlText.innerHTML += `?name=${nameInput.value}&year=${yearInput.value}`;
+    }
+} )
